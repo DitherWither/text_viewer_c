ce104fdfc80d323b2a298f895d64de514bb77994
DitherWither <demonslayervardhan@gmail.com>
2023-03-10 15:03:48 +0530

    Replace the deprecated flags with the new replacement

--- src/main.c
+++ src/main.c
@@ -35,7 +35,7 @@ main (int   argc,
 	bind_textdomain_codeset (GETTEXT_PACKAGE, "UTF-8");
 	textdomain (GETTEXT_PACKAGE);
 
-	app = text_viewer_c_application_new ("com.example.TextViewerC", G_APPLICATION_FLAGS_NONE);
+	app = text_viewer_c_application_new ("com.example.TextViewerC", G_APPLICATION_DEFAULT_FLAGS);
 	ret = g_application_run (G_APPLICATION (app), argc, argv);
 
 	return ret;
